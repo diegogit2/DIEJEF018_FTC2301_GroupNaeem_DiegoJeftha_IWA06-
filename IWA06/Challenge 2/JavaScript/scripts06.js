40686f7a608832d974949bfceb4221ec573949da
diegogit2 <125981571+diegogit2@users.noreply.github.com>
2023-04-04 15:18:48 +0200

    completed IWA06 challenges

---
+++ IWA06/Challenge 2/JavaScript/scripts06.js
@@ -0,0 +1,23 @@
+
+
+const rent = 400;
+const tax = '12%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 0;
+const minuteOfDay = 0;
+
+
+// Only change below this line
+
+if ((hourOfDay !== null )&& (minuteOfDay !== null) && (hourOfDay == 0) && (minuteOfDay == 0)) {
+    const taxAsDecimal = parseInt(tax) / 100
+    const startingAfterTax = salary * (1 - taxAsDecimal)
+    const balance = startingAfterTax - transport - food - rent
+
+    console.log('R', balance.toFixed(2))
+}
+
+// R274.46.00
+
